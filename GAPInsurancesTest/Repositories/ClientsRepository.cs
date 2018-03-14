98b497bcad1ed584446c982ac52fc5527525745b
Miguel Mejia <mmejiam9206@outlook.com>
2018-03-14 01:31:26 -0500

    GAPInsuranceTest - Added repository classes to access Entity Framework

---
+++ GAPInsurancesTest/Repositories/ClientsRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GAPInsurancesTest.Models;
+
+namespace GAPInsurancesTest.Repositories
+{
+    public class ClientsRepository : IClientsRepository, IDisposable
+    {
+        private GAPInsurancesDBEntities db;
+
+        public ClientsRepository(GAPInsurancesDBEntities db)
+        {
+            this.db = db;
+        }
+
+        public void DeleteClient(int client_id)
+        {
+            Client client = this.db.Clients.Find(client_id);
+            this.db.Clients.Remove(client);
+        }
+
+        void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        Client IClientsRepository.GetClientById(int client_id)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerable<Client> IClientsRepository.GetClients()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IClientsRepository.InsertClient(Client client)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IClientsRepository.Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IClientsRepository.UpdateClient(Client client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GAPInsurancesTest/Repositories/IClientsRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GAPInsurancesTest.Models;
+
+namespace GAPInsurancesTest.Repositories
+{
+    interface IClientsRepository : IDisposable
+    {
+        IEnumerable<Client> GetClients();
+        Client GetClientById(int client_id);
+        void InsertClient(Client client);
+        void UpdateClient(Client client);
+        void DeleteClient(int client_id);
+        void Save();
+    }
+}
